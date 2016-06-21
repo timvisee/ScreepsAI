1bade2d1b791ce68f65f96ed0bff79241fbf9771
Tim Visée <timvisee@gmail.com>
2016-06-21 18:10:50 +0200

    Implement tick start and end events

--- src/core.js
+++ src/core.js
@@ -1,3 +1,5 @@
+var EVENTS = require('events');
+var eventHandler = require('eventHandler');
 var gc = require('gc');
 
 /**
@@ -11,7 +13,13 @@ module.exports = {
      * Called on server tick.
      */
     tick: function() {
+        // Fire the tick start event
+        eventHandler.fire(EVENTS.EVENT_TICK_START);
+
         // Garbage collection
         gc.gc();
+
+        // Fire the tick end event
+        eventHandler.fire(EVENTS.EVENT_TICK_START);
     }
 };
