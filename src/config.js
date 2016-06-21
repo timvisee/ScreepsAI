d62015d212e0efda58e15bdf6df05cc188b6a5c2
Tim Visée <timvisee@gmail.com>
2016-06-21 18:05:15 +0200

    Create basic config

---
+++ src/config.js
@@ -0,0 +1,25 @@
+/**
+ * Config.
+ *
+ * @type {{}}
+ */
+module.exports = {
+    creeps: {
+        'harvester': {
+            id: 1,
+            count: 4
+        },
+        'upgrader': {
+            id: 2,
+            count: 4
+        },
+        'builder': {
+            id: 3,
+            count: 5
+        },
+        'fixer': {
+            id: 4,
+            count: 2
+        }
+    }
+};
