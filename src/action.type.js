afa203a8458f56ebbbfc0b78612672f3d3ee3e74
Tim Visée <timvisee@gmail.com>
2016-06-23 16:12:01 +0200

    Create action type configuration

---
+++ src/action.type.js
@@ -0,0 +1,18 @@
+// Variable used to determine action type indices
+var i = 0;
+
+/**
+ * List of action types.
+ *
+ * @type {*}
+ */
+module.exports = {
+
+    /**
+     * Action to move to a specified point.
+     */
+    ACTION_MOVE_TO: {
+        id: i++,
+        file: 'moveTo'
+    }
+};
