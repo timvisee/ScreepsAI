4119c253042113281e4bc5b34cc0725f464f3a5f
Tim Visée <timvisee@gmail.com>
2016-06-28 14:53:18 +0200

    Add module.exports prototype to module file

--- src/lib/module.js
+++ src/lib/module.js
@@ -4,3 +4,17 @@
  * @param {string} path Path to require.
  */
 function require(path) { };
+
+/**
+ * Module global.
+ *
+ * @type {{exports: *}}
+ */
+var module = {
+    /**
+     * Module to export.
+     *
+     * @type {*}
+     */
+    exports: undefined
+};
