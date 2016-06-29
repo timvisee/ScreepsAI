6d86080ab9564d799fdbe1390d38ebbe9509dc8d
Tim Visée <timvisee@gmail.com>
2016-06-29 16:37:01 +0200

    Create action logic file (test)

---
+++ src/action.logic.moveTo.js
@@ -0,0 +1,14 @@
+/**
+ * Create and export the action.
+ *
+ * @type {*}
+ */
+module.export = {
+
+    /**
+     * Called on action tick.
+     */
+    tick: function() {
+        console.log('Action logic \'moveTo\' called called!');
+    }
+};
